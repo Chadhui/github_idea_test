04b96b642c1f935a6d7b4eb4e362efe4e4b527a1
Chadhui <mr_liuyehui@163.com>
2020-05-12 20:33:30 +0800

    这是hui的提交

--- src/main/java/com/itstuyd/git/GithubIdea.java
+++ src/main/java/com/itstuyd/git/GithubIdea.java
@@ -3,5 +3,9 @@ package com.itstuyd.git;
 public class GithubIdea {
     public static void main(String[] args) {
         System.out.println("这是测试程序");
+        System.out.println("bbbbbbbbb");
+        System.out.println("bbbbbbbbb");
+        System.out.println("bbbbbbbbb");
+        System.out.println("bbbbbbbbb");
     }
 }
